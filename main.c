a4078e601f7d0ac2baa3bd8d5a73c7ba4ae053cc
Iskander <gasimov9898@gmail.com>
2023-09-07 15:47:14 +0300

    Give a personalized hello.

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 int main (int argo, char ++argv)
 {
-printf ("Hello wodld!\n")
-return 1;
+printf("Enter your name: ");
+fgets(name, 255, stdin);
+printf("length = %d\n", strlen(name));/* debug line*/
+name[strlen(name)-1] ='\0';/*remove the newline at the and*/
+
+printf("Hello %s!\n", name);
+return 0;
 }
