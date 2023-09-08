8b382bfa83331c864f80aec96bb70499af54fa33
Iskander <gasimov9898@gmail.com>
2023-09-08 12:21:56 +0300

    s

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main (int argo, char ++argv)
 {
 char first[255,] last [255];
+
 printf("Enter your first name: ");
 fgets(first, 255, stdin);
 first[strlen(first)-1]='\0'; /* remove the newline at the end*/
